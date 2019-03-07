496662a0f74016880772d1b89fab83a48677ef17
Tiago489 <tiagolopes489@gmail.com>
2019-03-06 20:44:38 -0500

    add documentation

--- web/app.py
+++ web/app.py
@@ -40,13 +40,15 @@ class Visit(Resource):
         VisitCounter.update({}, {"$set":{"num_of_visits":new_count}})
         #displays to the user how many times our service has been visited
         return str("Hello user, just fyi, this API service has been used " + str(new_count) + " times.")
-#define a check for the posted data
+# function that checks if user is sending in 2 values to be calculated as well as check to see if the user is trying to divide by zero
 def checkPostedData(postedData, functionName):
     if functionName == "add" or functionName == "subtract" or functionName == "multiply":
+        #if either x or y are not present in the postedData, throws error to user
         if "x" not in postedData or "y" not in postedData:
             return str("You need to send 2 numbers my way if you want me to calculate them.")
         else:
             return 200
+    #throws error if user tries to divide by zero
     elif functionName == "divide":
         if "x" not in postedData or "y" not in postedData:
             return str("You need to send 2 numbers my way if you want me to calculate them.")
@@ -57,11 +59,16 @@ def checkPostedData(postedData, functionName):
         else:
             return 200
 
+#define the Add class
 class Add(Resource):
+    #define how the app is to behave when a POST request calls this service
     def post(self):
+        #.get_json parses and returns the data as json
         postedData = request.get_json()
+        #calls the checkPostedData within the context of the add method
         status_code = checkPostedData(postedData, "add")
 
+        #handles error messages
         if status_code != 200:
             retJson = {
                 'Message': "There was an error",
@@ -69,17 +76,22 @@ class Add(Resource):
             }
             return jsonify(retJson)
 
+        #asigns the values of x and y entered by the user to local x and y variables
         x = postedData["x"]
         y = postedData["y"]
 
+        #ensures x and y are integers
         x = int(x)
         y = int(y)
 
+        #handles the actual arithmetic operation
         ret = x + y
+        #prepares the json response
         retMap = {
             'Message': ret,
             'Status Code' : 200
         }
+        #returns jsonified version of the response
         return jsonify(retMap)
 
 #define class Subtract
@@ -158,16 +170,18 @@ class Multiply(Resource):
         }
         return jsonify(retMap)
 
-#add resources
+#add resources to the api, that the user can call on by going to the url listed in the parantheses
 api.add_resource(Add, "/add")
 api.add_resource(Subtract, "/subtract")
 api.add_resource(Divide, "/divide")
 api.add_resource(Multiply, "/multiply")
 api.add_resource(Visit, "/")
+
 #define root route
 @app.route('/')
 def hello_world():
     return "Hello World"
 
+#initiates the application and runs it in the given port for Docker
 if __name__=="__main__":
     app.run(host='0.0.0.0')
